46006d5977ad127213237df48e56acb58ca3fb87
dits1019 <jdits1019@gmail.com>
2021-04-01 23:46:46 +0900

    이벤트 생성(미완성)

--- lib/homePage.dart
+++ lib/homePage.dart
@@ -53,6 +53,26 @@ class _HomePageState extends State<HomePage> {
               height: 900,
               child: NewMenuCard(),
             ),
+            Container(
+              margin: EdgeInsets.only(left: 10, right: 10),
+              height: 300,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(5), //border corner radius
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.5), //color of shadow
+                    spreadRadius: 1, //spread radius
+                    blurRadius: 10, // blur radius
+                    offset: Offset(0, 2), // changes position of shadow
+                    //first paramerter of offset is left-right
+                    //second parameter is top to down
+                  ),
+                  //you can set more BoxShadow() here
+                ],
+              ),
+            ),
           ],
         ),
       ),
