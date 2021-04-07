200a8cb35da707e851c27721ad2f8774c84ec64b
dits1019 <jdits1019@gmail.com>
2021-04-07 21:30:55 +0900

    Pay 페이지 완성

--- lib/payPage.dart
+++ lib/payPage.dart
@@ -1,3 +1,4 @@
+import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_starbucks/homePageWidget.dart';
 
@@ -11,6 +12,7 @@ class PayPage extends StatelessWidget {
           SliverAppBar(
             expandedHeight: 110,
             pinned: true,
+            centerTitle: true,
             backgroundColor: Colors.white,
             flexibleSpace: FlexibleSpaceBar(
               title: Text(
@@ -19,21 +21,76 @@ class PayPage extends StatelessWidget {
               ),
             ),
           ),
-          SliverFillRemaining(
-            child: ShadowContainer(
-                260,
-                Column(
-                  children: [
-                    Container(
-                      width: 200,
-                      height: 150,
-                      color: Colors.amber,
-                    ),
-                  ],
-                )),
+          SliverList(
+            delegate: new SliverChildListDelegate(_buildList(1)),
           )
         ],
       ),
     );
   }
+}
+
+List _buildList(int count) {
+  List<Widget> listItems = List();
+
+  for (int i = 0; i < count; i++) {
+    listItems.add(
+      new Padding(
+        padding: const EdgeInsets.only(top: 5),
+        child: Container(
+          width: 600,
+          height: 700,
+          color: Colors.white,
+          padding: const EdgeInsets.only(top: 5),
+          child: Padding(
+            padding: const EdgeInsets.only(bottom: 200),
+            child: ShadowContainer(
+                50,
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(top: 15),
+                      child: DottedBorder(
+                        color: Colors.grey,
+                        radius: Radius.circular(10),
+                        dashPattern: [8, 4],
+                        strokeWidth: 2,
+                        borderType: BorderType.RRect,
+                        child: Container(
+                          color: Colors.grey[100],
+                          width: 280,
+                          height: 170,
+                          child: Center(
+                            child: Icon(
+                              Icons.add_circle_outline,
+                              color: Colors.grey,
+                              size: 40,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 25, bottom: 10),
+                      child: Text(
+                        '스타벅스 카드를 등록해보세요.',
+                        style: TextStyle(
+                            fontSize: 19, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    Text(
+                      '매장과 사이렌오더에서 쉽고 편리하게\n사용할 수 있고, 별도 적립할 수 있습니다.',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(height: 1.5, color: Colors.grey[700]),
+                    ),
+                  ],
+                )),
+          ),
+        ),
+      ),
+    );
+  }
+
+  return listItems;
 }
